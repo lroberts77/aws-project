49426d86e9947826c24ec55184b07571daf81894
Liam R <liamroberts77@googlemail.com>
2021-06-27 20:03:55 +0100

    imported amplify and graphql

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react'
+import Amplify, { API, graphqlOperation } from 'aws-amplify'
 import './App.css';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
     </div>
