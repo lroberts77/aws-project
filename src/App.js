d8b6f78cc7e72682b987c582716da5bfa1b9ffaa
Liam R <liamroberts77@googlemail.com>
2021-06-27 20:14:43 +0100

    added usestate for todos

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ const initialState = { name: '', description: '' }
 const App = () => {
 
   const [formState, setFormState] = useState(initialState)
+  const [todos, setTodos] = useState([])
 
   return (
     <div className="App">
