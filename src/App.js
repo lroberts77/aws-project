39b5c7f306f3ad472ae4862a3df44f0413923a18
Liam R <liamroberts77@googlemail.com>
2021-06-27 20:05:33 +0100

    imported graphql queries, listtodos

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import Amplify, { API, graphqlOperation } from 'aws-amplify'
 import { createTodo } from './graphql/mutations'
-
+import { listTodos } from './graphql/queries'
 import './App.css';
 
 const App = () => {
