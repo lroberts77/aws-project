c59b962d0f3dc9f4b2d95864a350ea37cc6611e6
Liam R <liamroberts77@googlemail.com>
2021-07-02 21:45:40 +0100

    imported withautheticator in app.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Amplify, { API, graphqlOperation } from 'aws-amplify'
 import { createTodo } from './graphql/mutations'
 import { listTodos } from './graphql/queries'
 import './App.css';
+import { withAuthenticator } from '@aws-amplify/ui-react'
 
 import awsExports from "./aws-exports";
 Amplify.configure(awsExports);
